58bc6eefd55eade9a84d9a7c09f621c58d8daddd
plsletmeee <epic.game694@gmail.com>
2023-06-14 15:31:19 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    "token": "",
-    "prefix": "",
+    "token": "ODg5NjE4NjQ4MjEyMzI4NTE5.Gw29I6.awiHvN5ivkZN2GJ-IU7GtXj-iepbb_a0rQLBxk",
+    "prefix": "g.",
     "MEMER_API_TOKEN": "", // Get The Memer API Token from https://discord.com/invite/emD44ZJaSA
     "TENOR_API_KEY": "", // Get The Tenor API Key from https://www.tenor.co/api/v1/key/
-    "mongooseConnectionString": "", 
-    "id": "",  // https://discordapp.com/developers/applications/ID/information,
-    "clientSecret": "",  // https://discordapp.com/developers/applications/ID/information,
+    "mongooseConnectionString": "mongodb+srv://google:BdiQQTHstmUjh8Mv@cluster1.ooz2a.mongodb.net/yuh?retryWrites=true&w=majority", 
+    "id": "889618648212328519",  // https://discordapp.com/developers/applications/ID/information,
+    "clientSecret": "We0qpnvFeNAFzEY16kFpu5e95C3biMNB",  // https://discordapp.com/developers/applications/ID/information,
     "domain": "http://localhost:3000",
     "port": 3000,
     "usingCustomDomain": false
